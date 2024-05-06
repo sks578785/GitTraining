55c0e65f94ccdbed166e09b26f1ef173cc2aec1b
sks578785 <er.shahane@gmail.com>
2024-05-06 15:03:05 +0530

    first commit for add and sub.

---
+++ src/mathOps/Perform.java
@@ -0,0 +1,20 @@
+package mathOps;
+
+public class Perform {
+public static void main(String[] args) {
+	int a=23,b=12;
+	sum(a,b);
+	sub(a,b);
+}
+
+private static void sub(int a, int b) {
+	// TODO Auto-generated method stub
+	System.out.println("Substraction "+(a-b));
+}
+
+private static void sum(int a, int b) {
+	// TODO Auto-generated method stub
+	System.out.println("Addition " +(a+b));
+	
+}
+}
